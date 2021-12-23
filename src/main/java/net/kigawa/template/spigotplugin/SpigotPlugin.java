a15644b797c79710774880c417bfe8c0a12da57b
kigawa <kigawa.8390@gmail.com>
2021-12-24 00:01:30 +0900

    Initial commit

---
+++ src/main/java/net/kigawa/template/spigotplugin/SpigotPlugin.java
@@ -0,0 +1,8 @@
+package net.kigawa.template.spigotplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotPlugin extends JavaPlugin {
+
+
+}
---
+++ src/test/java/SpigotRunner.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class SpigotRunner {
+    private static final SpigotRunner RUNNER;
+    private static final String VERSION = "1.16.5";
+
+    static {
+        RUNNER = new SpigotRunner();
+    }
+
+    private File spigotDir;
+
+    public static void main(String[] args) {
+
+    }
+}
